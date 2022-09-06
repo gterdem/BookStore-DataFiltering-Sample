702340f9a3a9f3c5b64126ae8efb55741ebb7cb1
Galip Tolga Erdem <galiperdem@gmail.com>
2022-09-06 16:14:04 +0300

    Added custom book repository implementation

---
+++ src/BookStore.EntityFrameworkCore/Books/EfCoreBooksRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BookStore.EntityFrameworkCore;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace BookStore.Books;
+
+public class EfCoreBooksRepository : EfCoreRepository<BookStoreDbContext, Book, Guid>, IBookRepository
+{
+    public EfCoreBooksRepository(IDbContextProvider<BookStoreDbContext> dbContextProvider) : base(dbContextProvider)
+    {
+    }
+}
